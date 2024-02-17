9c02181d5f61f20e3a83bda216ac070bd564acc2
Chirag Raju <chiragrajus2002@gmail.com>
2024-02-17 22:44:34 +0530

    Basic Concepts

---
+++ fibbanacci.js
@@ -0,0 +1,19 @@
+const fib = (n) => {
+  const arr = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 1] + arr[i - 2]);
+  }
+  return arr[n];
+};
+const res1 = fib(9);
+console.log(res1); //34
+
+//using recursion
+const fibb = (n) => {
+  if (n <= 1) {
+    return n;
+  }
+  return fibb(n - 1) + fibb(n - 2);
+};
+
+console.log(fibb(9)); //34
