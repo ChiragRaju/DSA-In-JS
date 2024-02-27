9eb44d6dfb1ba03f77563190aa2e508fc3400b8d
Chirag Raju <chiragrajus2002@gmail.com>
2024-02-28 00:04:35 +0530

    buysellstocks

--- Arrays.js
+++ Arrays.js
@@ -10,6 +10,8 @@ const twoSum = (num, target) => {
 const res = twoSum([1, 2, 3], 5);
 console.log(res);
 
+//optimized
+
 const twoSums = (num, target) => {
   obj = {};
   for (let i = 0; i < num.length; i++) {
@@ -22,3 +24,39 @@ const twoSums = (num, target) => {
   }
 };
 console.log(twoSums([2, 3, 1], 4));
+
+//best time to buy sell stocks
+
+function maxProfit(prices) {
+  debugger;
+  let globalProfit = 0;
+  for (let i = 0; i < prices.length - 1; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      const currProfit = prices[j] - prices[i];
+      if (currProfit > globalProfit) {
+        globalProfit = currProfit;
+      }
+    }
+  }
+  return globalProfit;
+}
+
+console.log(maxProfit([7, 6, 4, 3, 1]));
+
+//greedy algorithm
+const buySellStock = (prices) => {
+  let minimum = prices[0] || 0;
+  let profit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i < minimum]) {
+      minimum = prices[i];
+    }
+    profit = Math.max(profit, prices[i] - minimum);
+  }
+};
+console.log(buySellStock([7, 6, 4, 3, 1]));
+
+//explanation
+// arr = [7,1,5,3,6,4];
+// min=7=>1
+// profit=0=>5-1=>4
